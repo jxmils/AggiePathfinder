5e65a90d4a4f4b9a09ac645ab613bd2867cfe32a
Jason Paul Miller <jamiller@cs.nmsu.edu>
2023-04-07 01:05:00 -0600

    better design

--- AggiePathfinder/AggiePathfinder/LocationScreen.swift
+++ AggiePathfinder/AggiePathfinder/LocationScreen.swift
@@ -45,58 +45,77 @@ struct SearchBarContainer: View {
     @Binding var destinationSearchText: String
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack {
-                Text("Location")
-                    .font(.headline)
-                Spacer()
-                Spacer()
-                Spacer()
-                TextField("Search", text: $myLocationSearchText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+        ZStack {
+            Capsule()
+                .fill(Color(red: 0.961, green: 0.957, blue: 0.922))
+                .shadow(color: Color(red: 0.2, green: 0.2, blue: 0.2), radius: 8, x: 0, y: 4)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    Text("Location")
+                        .font(.headline)
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    TextField("Search", text: $myLocationSearchText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                HStack {
+                    Text("Destination")
+                        .font(.headline)
+                    Spacer()
+                    TextField("Search", text: $destinationSearchText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
             }
-            HStack {
-                Text("Destination")
-                    .font(.headline)
-                Spacer()
-                TextField("Search", text: $destinationSearchText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
+            .padding()
         }
-        .padding()
-        .background(Color(red: 0.961, green: 0.957, blue: 0.922))
-        .cornerRadius(10)
         .frame(height: UIScreen.main.bounds.height * 0.2)
     }
 }
 
 struct LocationScreen: View {
     @StateObject private var locationManager = LocationManager()
-    @State private var searchText: String = ""
-
+    @State private var myLocationSearchText: String = ""
+    @State private var destinationSearchText: String = ""
+    @State private var isSearching = false
+    
     var body: some View {
         ZStack {
             MapView(locationManager: locationManager)
                 .edgesIgnoringSafeArea(.all)
-
+            
             VStack {
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                    TextField("Search for a location", text: $searchText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                NavigationLink(destination: SearchScreen(locationManager: locationManager, isSearching: $isSearching), isActive: $isSearching) {
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 32, height: 32)
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                        }
+                        TextField("Search for a location", text: $myLocationSearchText)
+                            .padding(.leading, 8)
+                            .foregroundColor(.black)
+                    }
+                    .padding(.horizontal, 16)
+                    .frame(height: 40)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .padding(.top, 16)
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal, 16)
-                .frame(height: 40)
-                .background(Color.white)
-                .cornerRadius(8)
-                .padding(.top, 16)
-                .padding(.horizontal)
-
+                
                 Spacer()
             }
+            .opacity(isSearching ? 0 : 1)
+            .animation(.default)
         }
     }
 }
+
 
 struct LocationScreen_Previews: PreviewProvider {
     static var previews: some View {
